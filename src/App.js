5f81898d8895758c35be326b34e61f036a8a4bc2
Aman Goyal <im.developer26@gmail.com>
2020-10-31 20:43:25 +0530

    sidebar done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
   return (
     <div className="app">
-      <h2>Welcome to discord clone</h2>
+
+      <Sidebar />
+
     </div>
   );
 }
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,66 @@
+import { Avatar } from '@material-ui/core';
+import { Add, Call, ExpandMore, Headset, InfoOutlined, Mic, Settings, SignalCellularAlt } from '@material-ui/icons';
+import React from 'react';
+import SidebarChannel from '../SidebarChannel/SidebarChannel';
+import './Sidebar.css';
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebar__top">
+                <h3>Aman Goyal</h3>
+                <ExpandMore />
+            </div>
+
+            <div className="sidebar__channels">
+                <div className="sidebar__channelsHeader">
+                    <div className="sidebar__header">
+                        <ExpandMore />
+                        <h4>Text Channel</h4>
+                    </div>
+
+                    <Add className="sidebar__addChannel" />
+                </div>
+
+                <div className="sidebar__channelsList">
+                    <SidebarChannel />
+                    <SidebarChannel />
+                    <SidebarChannel />
+                    <SidebarChannel />
+                </div>
+            </div>
+
+            <div className="sidebar__voice">
+                <SignalCellularAlt
+                    className="sidebar__voiceIcon"
+                    fontSize="large"
+                />
+                <div className="sidebar__voiceInfo">
+                    <h3>Voice Connected</h3>
+                    <p>Stream</p>
+                </div>
+
+                <div className="sidebar__voiceIcons">
+                    <InfoOutlined />
+                    <Call />
+                </div>
+            </div>
+
+            <div className="sidebar__profile">
+                <Avatar src="https://avatars3.githubusercontent.com/u/45818629?v=4" />
+                <div className="sidebar__profileInfo">
+                    <h3>Aman Goyal</h3>
+                    <p>#5540</p>
+                </div>
+
+                <div className="sidebar__profileIcons">
+                    <Mic />
+                    <Headset />
+                    <Settings />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
