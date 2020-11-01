a91696ee5f622a0690ada0b610da9c57bf963b5b
Aman Goyal <im.developer26@gmail.com>
2020-11-01 23:59:48 +0530

    chat input

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,3 +1,4 @@
+import { AddCircle, CardGiftcard, EmojiEmotions, Gif } from '@material-ui/icons';
 import React from 'react';
 import ChatHeader from '../ChatHeader/ChatHeader';
 import './Chat.css';
@@ -6,6 +7,29 @@ function Chat() {
     return (
         <div className="chat">
             <ChatHeader />
+
+            <div className="chat__messages">
+
+            </div>
+
+            <div className="chat__input">
+                <AddCircle fontSize="large" />
+
+                <form >
+                    <input placeholder={`Message #Test channel name`}></input>
+                    <button className="chat__inputButton" type="submit">
+                        Send Message
+                    </button>
+                </form>
+
+                <div className="chat__inputIcons">
+                    <CardGiftcard fontSize="large" />
+                    <Gif fontSize="large" />
+                    <EmojiEmotions fontSize="large" />
+                </div>
+
+            </div>
+
         </div>
     )
 }
